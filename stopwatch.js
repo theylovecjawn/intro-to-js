9a87f9d668c3f6446797c6d63ca6aec3c69c33c7
theylovecjawn <calebjohn234@gmail.com>
2023-04-28 14:56:00 -0400

    Stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,8 @@
+function start() {
+    alert("You have clicked the start button.")}
+function stop(){
+    alert("You have clicked the stop button.")
+}
+function reset(){
+    alert("You have clicked the reset button")
+}

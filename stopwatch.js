f7b2c200b1362d1d79a352fe138a5cf026bf3f65
theylovecjawn <calebjohn234@gmail.com>
2023-05-01 14:43:49 -0400

    Fixed Stop watch!

--- stopwatch.js
+++ stopwatch.js
@@ -1,8 +1,34 @@
+let seconds = 0
+let milliseconds = 0
+let minutes = 0
+let digits = document.getElementById('digits')
+let interval;
 function start() {
-    alert("You have clicked the start button.")}
+    reset()
+
+    interval = setInterval(function (){
+        milliseconds = milliseconds + 10
+    if (milliseconds >= 1000){
+        seconds = seconds + 1
+        milliseconds = 0
+    }
+    if(seconds>= 60){
+        minutes = minutes + 1
+        seconds = 0
+    }
+    
+    digits.innerHTML = String(minutes).padStart(2, '0') + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds / 10
+
+},10)
+}
 function stop(){
-    alert("You have clicked the stop button.")
+   clearInterval(interval)
 }
 function reset(){
-    alert("You have clicked the reset button")
+   stop()
+   milliseconds = 0
+   seconds = 0
+   minutes = 0
+   digits.innerHTML = "00:00:00"   
+
 }

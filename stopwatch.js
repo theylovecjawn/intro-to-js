8da8b5f7ebb2c398d31c642ee9c7359d8293a5c3
theylovecjawn <calebjohn234@gmail.com>
2023-05-02 14:48:09 -0400

    finalized the stop watch with the lap

--- stopwatch.js
+++ stopwatch.js
@@ -3,9 +3,13 @@ let milliseconds = 0
 let minutes = 0
 let digits = document.getElementById('digits')
 let interval;
+let startButton = document.getElementById("start-btn")
+let stopButton = document.getElementById("stop-btn")
 function start() {
-    reset()
 
+    startButton.disabled = true 
+    stopButton.disabled = false
+    
     interval = setInterval(function (){
         milliseconds = milliseconds + 10
     if (milliseconds >= 1000){
@@ -23,12 +27,25 @@ function start() {
 }
 function stop(){
    clearInterval(interval)
+   stopButton.disabled = true
+   startButton.disabled = false
+    
 }
 function reset(){
    stop()
    milliseconds = 0
    seconds = 0
    minutes = 0
-   digits.innerHTML = "00:00:00"   
+   digits.innerHTML = "00:00:00"  
+   const lapList = document.getElementById("lapList1")
+   lapList.innerHTML = ""
 
 }
+function addlap(){
+    let newListItem = document.createElement("li")
+    newListItem.textContent = digits.textContent
+    const lapList = document.getElementById("lapList1")
+    lapList.appendChild(newListItem);
+}
+
+    
